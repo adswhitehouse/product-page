e6245823db5bebe68643d8df157d220f2f9f6398
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-03-04 15:00:00 +0000

    created form in html and amended code

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 // Variables
-let sizeOption = document.querySelectorAll(".jsSizeOption");
 let imageSelection = document.querySelectorAll(".jsImageSelection");
 let mainImage = document.querySelector(".jsMainImage");
-
-// Highlights size selection
-sizeOption.forEach((option) => {
-  option.addEventListener("click", () => {
-    sizeOption.forEach((option) => {
-      option.style.color = "#242021";
-    });
-    option.style.color = "rgb(71, 64, 208)";
-  });
-});
 
 // Changes product image and highlights image selector button
 imageSelection.forEach((selection) => {
